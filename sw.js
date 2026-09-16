1f10771f6ff0d810240977c6f027f0000b467c60
mrpotato <aryahanifdiyanath@gmail.com>
2026-09-16 22:22:12 +0700

    Force PWA cache refresh v3

--- sw.js
+++ sw.js
@@ -1,8 +1,12 @@
-const CACHE = 'daily-os-v2';
-const ASSETS = ['./','./index.html','./styles.css','./app.js','./db.js','./manifest.json','./icons/icon.svg','./icons/icon-180.png','./icons/icon-512.png'];
+const CACHE = 'daily-os-v3';
+const ASSETS = ['./','./index.html','./styles.css?v=3','./app.js?v=3','./db.js','./manifest.json','./icons/icon.svg','./icons/icon-180.png','./icons/icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
-  e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+  e.respondWith(fetch(e.request).then(res=>{
+    const copy=res.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,copy));
+    return res;
+  }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
 });
